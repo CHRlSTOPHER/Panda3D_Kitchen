20303cc5a8bd37462c028d94b2af851f1bcb497f
Chris541500 <chriscapaldi1990@gmail.com>
2023-12-26 00:21:17 -0500

    master_editor: ability to leave out-
    -node transform printer + gui

--- classes/editors/MasterEditor.py
+++ classes/editors/MasterEditor.py
@@ -14,26 +14,28 @@ from classes.gui.MouseWindowLock import MouseWindowLock
 
 class MasterEditor(DirectObject):
 
-    def __init__(self, mouse_lock=True, rot_cam=True, fov=50):
+    def __init__(self, mouse_lock=True, rot_cam=True, nt_printer=True):
         DirectObject.__init__(self)
         self.hide_gui = False
         self.accept("`", self.hide_editor_gui)
 
         self.mouse_lock = None
         self.rot_cam = None
+        self.nt_printer = None
+        self.nt_gui = None
+
         self.fov_wheel = FovScrollWheel()
-        # Set camera as default node. It can be changed later by selection.
         self.node_mover = NodeMover(camera)
         self.node_selector = NodeSelector(self.node_mover)
-        self.nt_printer = NodeTransformPrinter(*get_transform_data(camera))
 
         if mouse_lock:
             self.mouse_lock = MouseWindowLock()
         if rot_cam:
             self.rot_cam = RotationalCamera()
-
-        self.nt_gui = NodeTransformPrinterGUI(
-            self.node_mover, self.nt_printer, base.a2dLeftCenter)
+        if nt_printer:
+            self.nt_printer = NodeTransformPrinter(*get_transform_data(camera))
+            self.nt_gui = NodeTransformPrinterGUI(
+                self.node_mover, self.nt_printer, base.a2dLeftCenter)
 
         self.gui_classes = [
             self.nt_gui, self.mouse_lock
@@ -53,12 +55,13 @@ class MasterEditor(DirectObject):
         return self.node_mover
 
     def cleanup(self):
-        classes = [self.node_mover, self.node_selector,
-                   self.fov_wheel, self.nt_gui]
+        classes = [self.node_mover, self.node_selector, self.fov_wheel]
         if self.mouse_lock:
             classes.append(self.mouse_lock)
         if self.orb_cam:
             classes.append(self.orb_cam)
+        if self.nt_printer:
+            classes.append(self.nt_printer)
 
         for class_item in classes:
             class_item.cleanup()
