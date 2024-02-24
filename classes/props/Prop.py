1e21acb64fc5f31c86cc30053f926cdb64ac752d
Chris541500 <chriscapaldi1990@gmail.com>
2024-02-23 19:42:49 -0500

    prop: child nodes

--- classes/props/Prop.py
+++ classes/props/Prop.py
@@ -5,10 +5,16 @@ class Prop(NodePath):
 
     def __init__(self, model_path, name="~prop", parent=None,
                  pos=(0, 0, 0), hpr=(0, 0, 0), scale=(1, 1, 1),
-                 color=(1, 1, 1, 1)):
-        NodePath.__init__(self, loader.loadModel(model_path))
+                 color=None, child=None):
+        if child:
+            NodePath.__init__(self,
+                              loader.loadModel(model_path).find(f'**/{child}'))
+        else:
+            NodePath.__init__(self, loader.loadModel(model_path))
+
         self.set_name(name)
         if parent:
             self.reparent_to(parent)
         self.set_pos_hpr_scale(*pos, *hpr, *scale)
-        self.set_color(*color)
+        if color:
+            self.set_color(*color)
