33cede44619ee7e1e29376a3a0155007bd0d9746
Chris541500 <chriscapaldi1990@gmail.com>
2024-03-18 22:32:48 -0400

    startup: reload modules with r
    rn all modules reload but eventually i only want to reload modules individually. example: you add new intervals to a sequence and only reload the scenes file.

--- projects/Toonys2024/Main.py
+++ projects/Toonys2024/Main.py
@@ -1,26 +1,39 @@
-from Actors import Actors
-from Dialogue import Dialogue
-from Music import Music
-from ParticleEffects import ParticleEffects
-from Props import Props
-from Scenes import Scenes
-from Sounds import Sounds
-from TextBoxes import TextBoxes
-from Textures import Textures
+import importlib
+
+from direct.showbase.DirectObject import DirectObject
+
+import Actors
+import Dialogue
+import Music
+import ParticleEffects
+import Props
+import Scenes
+import Sounds
+import TextBoxes
+import Textures
+
+module_list = (Actors, Dialogue, Music, ParticleEffects, Props,
+              Scenes, Sounds, TextBoxes, Textures)
 
 
-class Main():
+class Main(DirectObject):
 
     def __init__(self):
         self.load_project()
+        self.accept('r', self.reload_modules)
 
     def load_project(self):
-        self.actors = Actors()
-        self.dialogue = Dialogue()
-        self.music = Music()
-        self.particle_effects = ParticleEffects()
-        self.props = Props()
-        self.scenes = Scenes()
-        self.sounds = Sounds()
-        self.text_boxes = TextBoxes()
-        self.textures = Textures()
+        self.actors = Actors.Actors()
+        self.dialogue = Dialogue.Dialogue()
+        self.music = Music.Music()
+        self.particle_effects = ParticleEffects.ParticleEffects()
+        self.props = Props.Props()
+        self.scenes = Scenes.Scenes()
+        self.sounds = Sounds.Sounds()
+        self.text_boxes = TextBoxes.TextBoxes()
+        self.textures = Textures.Textures()
+
+    def reload_modules(self):
+        for module in module_list:
+            importlib.reload(module)
+        self.load_project()
